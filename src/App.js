23daf0309073e84e323ad5c439d877ca7056f687
Sandy Pestaña <sandy.milena7@gmail.com>
2022-06-28 10:18:44 -0500

    agregando los archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Comment, useState } from 'react'
+import Header from './Componentes/Header/Header';
+import ListCard from '../src/pages/ListCard/ListCard';
+import axios from 'axios';
 
-function App() {
+function App(props) {
+  const [query, setQuery] = useState({})
+  const [text, setText] = useState('')
+
+  const actualizar = (e) => {
+    setText(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault()
+    const url = 'http://localhost:3001/api/items'
+    const res = await axios.get(`${url}?q=${text}`)
+    setQuery(res.data)
+
+  }
+ 
+  console.log(query, 'data')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header 
+        actualizar={actualizar}
+        text={text}
+        handleOnSubmit={handleOnSubmit} 
+      />
+      <ListCard
+        query={query} // 1. Pasamos query que es props renombrada y la data que seria {query}. La prop query es la que pasaremos al componente ListCard
+      />
     </div>
   );
 }
